97c9cec6e4529307afbbc87dcf16947f04cae3c8
Ir1d <sirius.caffrey@gmail.com>
2020-01-30 14:04:02 +0800

    update sidebar, disable mdx option in theme to avoid _frontmatter error

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,18 @@ module.exports = {
       resolve: 'gatsby-theme-dox',
       options: {
         basePath: '/',
-        contentPath: 'docs/**/*'
+        contentPath: 'docs/**/*',
+        mdx: false 
       }
-    }
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        gatsbyRemarkPlugins: [],
+        remarkPlugins: [require("remark-math"), require("remark-details")],
+        rehypePlugins: [require('rehype-mathjax'), require('rehype-details')],
+        extensions: [".mdx", ".md"]
+      }
+    },
   ]
 }
