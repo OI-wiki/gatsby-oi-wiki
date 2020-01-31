f761f4369cf9df7727a02116466bc372c0a52575
sshwy <jy.cat@qq.com>
2020-01-31 16:11:01 +0800

    copy theme dox to src

---
+++ src/gatsby-theme-dox/templates/doc.js
@@ -0,0 +1,23 @@
+import { graphql } from 'gatsby'
+import React from 'react'
+import Doc from '../components/doc'
+
+export default ({ data, location }) => {
+  return <Doc data={data} location={location} />
+}
+
+export const query = graphql`
+  query($id: String!) {
+    doc: doc(id: { eq: $id }) {
+      id
+      slug
+      title
+      description
+      excerpt
+      body
+      headings {
+        value
+      }
+    }
+  }
+`
