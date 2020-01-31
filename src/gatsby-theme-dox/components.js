66e252223f887dc732169b6b24f84279ee87c59f
sshwy <jy.cat@qq.com>
2020-01-31 15:46:30 +0800

    fix bugs of relative links

--- src/gatsby-theme-dox/components.js
+++ src/gatsby-theme-dox/components.js
@@ -1,6 +1,8 @@
 import components from 'gatsby-theme-dox/src/components'
 import Img from './components/Img'
+import Link from './components/Link'
 
 export default {
-    ...components
+    ...components,
+    a: Link,
 }
