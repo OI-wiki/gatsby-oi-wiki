adc09c34ba341eadabc84e2378d7c609ed8817a2
sshwy <jy.cat@qq.com>
2020-02-04 13:14:32 +0800

    fix bugs of  details notes (icon and ???+"...")

---
+++ src/gatsby-theme-dox/components/Details.js
@@ -0,0 +1,28 @@
+/** @jsx jsx */
+import { Link as GatsbyLink } from 'gatsby'
+import { jsx } from 'theme-ui'
+
+const detailsStyle = {
+    boxShadow: '0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12), 0 3px 1px -2px rgba(0,0,0,.2)',
+    position: 'relative',
+    margin: '1.5625em 0',
+    padding: '0 .6rem',
+    borderLeft: '.3rem solid #448aff',
+    borderRadius: '.1rem',
+    overflow: 'auto',
+    fontSize: '80%'
+}
+
+export default function({ className = '', children, ...props }){
+    return className.match('open')? (
+        <details className={className} sx={detailsStyle} {...props} open> 
+            {children}
+        </details>
+    ) : (
+        <details className={className} sx={detailsStyle} {...props} >
+            {children}
+        </details>
+    )
+}
+
+
