b40864cbb2c106de46c8b6292d012af9cdb48b09
sshwy <jy.cat@qq.com>
2020-02-05 10:39:11 +0800

    support loading mathjax while routing

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,12 @@
-import './src/styles/global.css'
+const React = require("react")
+
+// Logs when the client route changes
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+    //console.log("old pathname", prevLocation ? prevLocation.pathname : null);
+    //console.log("new pathname", location.pathname);
+    (function () {
+        //reload mathjax
+        window.MathJax.Hub.Typeset();
+    })();
+}
+
