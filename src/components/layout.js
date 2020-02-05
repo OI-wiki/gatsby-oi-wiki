9b400f67ed93ca32f4ea05e664f19fe7fa1fa4b8
Ir1d <sirius.caffrey@gmail.com>
2020-02-05 18:57:51 +0800

    wrap layout in antd

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,39 +1,57 @@
 /** @jsx jsx */
-// import SEO from '../components/SEO'
-import { Global } from '@emotion/core'
-// import { Helmet } from 'react-helmet'
-import { jsx } from 'theme-ui'
-// import { css, jsx, Styled, useThemeUI } from 'theme-ui'
-// import useSidebar from '../hooks/useSidebar'
-import Footer from './Footer'
-import Navbar from './Navbar'
-// import ScreenReader from './ScreenReader'
-// import Sidebar from './Sidebar'
-// import SidebarToggler from './Sidebar/Toggler'
-import AuthorsArray from './AuthorsArray'
-import Tags from './Tags'
-import Toc from './Toc'
+import { Global } from "@emotion/core";
+import { jsx } from "theme-ui";
+import myFooter from "./Footer";
+import Navbar from "./Navbar";
+import AuthorsArray from "./AuthorsArray";
+import Tags from "./Tags";
+import Toc from "./Toc";
+import Link from "./Link";
+import { Button, Layout, Menu, Breadcrumb, Icon, Card } from "antd";
+import SideBar from "./Sidebar";
+import "antd/dist/antd.css";
 
-function Layout({ children, location, authors, title, description, tags, toc}) {
+const { Header, Content, Footer, Sider } = Layout;
+
+function myLayout({
+  children,
+  location,
+  authors,
+  title,
+  description,
+  tags,
+  toc
+}) {
   return (
-    <div>
+    <Layout style={{ minHeight: "100vh" }}>
       <Global />
-      <div>
+      <Header>
         <Navbar />
+      </Header>
+      <Layout>
+        <Layout>
+          <SideBar />
 
-        <main>
-          {children}
-        </main>
+          <Content>
+            <Breadcrumb>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div>
+              <Card title={title}>{children}</Card>
+            </div>
+          </Content>
 
-        <AuthorsArray authors={authors} />
+          <AuthorsArray authors={authors} />
 
-        <Tags tags={tags} />
+          <Tags tags={tags} />
 
-        <Footer />
-      </div>
-      <Toc toc={toc} />
-  </div>
-  )
+          <Toc toc={toc} />
+          <Footer style={{ textAlign: "center" }}>{/* <myFooter /> */}</Footer>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
 }
 
-export default Layout
+export default myLayout;
