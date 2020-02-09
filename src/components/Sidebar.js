de0048e255f4b89045e8a95b311b7bf8b79d423e
sshwy <jy.cat@qq.com>
2020-02-09 14:39:19 +0800

    add overflow to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -82,7 +82,7 @@ export default function(props) {
         defaultSelectedKeys={[okey[0]]}
         defaultOpenKeys={okey}
         mode="inline"
-        style={{ height: "100%", borderRight: 0 }}
+        style={{ height: "100%", borderRight: 0, overflowY:"auto" }}
       >
         {pathList.map(item => Item(item))}
       </Menu>
