1fba4de283c9cebbafe5b9171d8b2aab5a9ea391
Ir1d <sirius.caffrey@gmail.com>
2020-02-09 10:32:16 +0800

    add codeblock to tests, comment out prism for now

--- src/components.js
+++ src/components.js
@@ -1,12 +1,12 @@
-import Prism from '@theme-ui/prism'
+// import Prism from '@theme-ui/prism'
 import Link from './components/Link'
 import Details from './components/Details'
 import Summary from './components/Summary'
 
 export default {
   a: Link,
-  code: Prism,
-  pre: props => props.children,
+  // code: Prism,
+  // pre: props => props.children,
   details: Details,
   summary: Summary,
 }
