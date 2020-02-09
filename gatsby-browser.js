3a9f20d95ff10101f4538ad40a6358bc682fa8a1
sshwy <jy.cat@qq.com>
2020-02-09 13:59:59 +0800

    try to fix sidebar

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,9 @@
 const React = require("react")
 
-// Logs when the client route changes
 exports.onRouteUpdate = ({ location, prevLocation }) => {
   //console.log("old pathname", prevLocation ? prevLocation.pathname : null);
   //console.log("new pathname", location.pathname);
-  ;(function() {
+  (function() {
     //reload mathjax
     window.MathJax.Hub.Typeset()
   })()
