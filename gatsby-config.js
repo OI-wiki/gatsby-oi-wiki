25f3862b21b1b0bbb4ec5db64746abc4fe08fa28
Ir1d <sirius.caffrey@gmail.com>
2020-02-11 16:00:48 +0800

    update config to make it work

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,25 +43,28 @@ module.exports = {
           //    toHeading: 6
           //  }
           //},
+          {
+            resolve: `gatsby-remark-vscode`,
+            options: {
+              theme: 'Dark+ (default dark)', // Read on for list of included themes. Also accepts object and function forms.
+              wrapperClassName: '',   // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
+              injectStyles: true,     // Injects (minimal) additional CSS for layout and scrolling
+              extensions: [],         // Extensions to download from the marketplace to provide more languages and themes
+              extensionDataDirectory: // Absolute path to the directory where extensions will be downloaded. Defaults to inside node_modules.
+                path.resolve('extensions'),
+              languageAliases: {},    // Map of custom/unknown language codes to standard/known language codes
+              replaceColor: x => x,   // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
+              getLineClassName: ({    // Function allowing dynamic setting of additional class names on individual lines
+                content,              //   - the string content of the line
+                index,                //   - the zero-based index of the line within the code fence
+                language,             //   - the language specified for the code fence
+                meta                  //   - any options set on the code fence alongside the language (more on this later)
+              }) => '',
+              logLevel: 'warn'       // Set to 'info' to debug if something looks wrong
+            }  
+          }
         ],
         remarkPlugins: [require("remark-math"), require("remark-details"),
-          [require("gatsby-remark-vscode"), {
-            theme: 'Dark+ (default dark)', // Read on for list of included themes. Also accepts object and function forms.
-            wrapperClassName: '',   // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
-            injectStyles: true,     // Injects (minimal) additional CSS for layout and scrolling
-            extensions: [],         // Extensions to download from the marketplace to provide more languages and themes
-            extensionDataDirectory: // Absolute path to the directory where extensions will be downloaded. Defaults to inside node_modules.
-              path.resolve('extensions'),
-            languageAliases: {},    // Map of custom/unknown language codes to standard/known language codes
-            replaceColor: x => x,   // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
-            getLineClassName: ({    // Function allowing dynamic setting of additional class names on individual lines
-              content,              //   - the string content of the line
-              index,                //   - the zero-based index of the line within the code fence
-              language,             //   - the language specified for the code fence
-              meta                  //   - any options set on the code fence alongside the language (more on this later)
-            }) => '',
-            logLevel: 'warn'       // Set to 'info' to debug if something looks wrong
-          }]
         ],
         rehypePlugins: [require("rehype-mathjax"), require("rehype-details")],
         extensions: [".mdx", ".md"],
