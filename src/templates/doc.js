9571e812a34a96c25d517c81e0b5169cf9c8aafc
sshwy <jy.cat@qq.com>
2020-02-19 07:53:40 +0800

    add global css

--- src/templates/doc.js
+++ src/templates/doc.js
@@ -1,6 +1,7 @@
 import { graphql } from "gatsby"
 import React from "react"
 import Doc from "../components/doc"
+import "./extra.css"
 
 export default ({ data, location }) => {
   // console.log(data)
