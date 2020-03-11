b11e58212e6c5c8f4fb1fab9b925bc41771786cc
Margatroid <i@margatroid.xyz>
2020-03-11 10:09:45 +0800

    fix: remove duplicate files and fix responsive nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import Link from "./Link"
 import { MdSchool } from "react-icons/md"
 import theme from "../theme"
 import classNames from 'classnames'
-import { Menu,Icon,Input } from 'antd'
+import { Menu, Icon, Input } from 'antd'
 // import algoliasearch from 'algoliasearch/lite';
 // import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 const { SubMenu } = Menu;
@@ -30,62 +30,62 @@ class Navbar extends React.Component {
       current: e.key,
     });
   };
-  render(){
-  return (
+  render() {
+    return (
 
-    <nav
-      aria-label="Navbar Menu"
-      sx={{
-        boxShadow:
-          "0 0 0.2rem rgba(0,0,0,.1), 0 0.2rem 0.4rem rgba(0,0,0,.2)",
-        width: "100%",
-        padding: "0 3%"
-      }}
-    >
-      <div
-        sx={{ color: "black" }}
-        className="nav-firstrow"
+      <nav
+        aria-label="Navbar Menu"
+        sx={{
+          boxShadow:
+            "0 0 0.2rem rgba(0,0,0,.1), 0 0.2rem 0.4rem rgba(0,0,0,.2)",
+          width: "100%",
+          padding: "0 3%"
+        }}
       >
-        <ul sx={theme.layout.navbar.navList}>
-          <Link
-            href='/'
-            sx={{
-              float: "left",
-              fontSize: "large",
-              color: "black",
-            }}
-          >
-            <MdSchool
-              className="logo"
-              size={30}
-              sx={{ m: "0.5rem 1rem 0.5rem 1rem" }}
-            ></MdSchool>
-          </Link>
-          <Link href='/'
-            sx={{
-              color: "text",
-              ":hover":{
-                color :"#1E90FF",
-                textDecoration: "none"
-              }
-            }}
-          >OI WiKi</Link>
-          <Menu
-            sx={{ float: "right" }}
-            className="layui-nav"
-            onClick={this.handleClick}
-            selectedKeys={[this.state.current]}
-            mode="horizontal"
-          >
-            <Menu.Item key="menu-search">
-              <Search
-                placeholder="键入进行搜索"
-                onSearch={value => /*console.log(value)*/ value}
-                style={{ width: 200 }}
-              />
-            </Menu.Item>
+        <div
+          sx={{ color: "black" }}
+          className="nav-firstrow"
+        >
+          <ul sx={theme.layout.navbar.navList}>
+            <Link
+              href='/'
+              sx={{
+                float: "left",
+                fontSize: "large",
+                color: "black",
+              }}
+            >
+              <MdSchool
+                className="logo"
+                size={30}
+                sx={{ m: "0.5rem 1rem 0.5rem 1rem" }}
+              ></MdSchool>
+            </Link>
+            <Link href='/'
+              sx={{
+                color: "text",
+                ":hover": {
+                  color: "#1E90FF",
+                  textDecoration: "none"
+                }
+              }}
+            >OI WiKi</Link>
+            <Menu
+              sx={{ float: "right" }}
+              onClick={this.handleClick}
+              selectedKeys={[this.state.current]}
+              mode="horizontal"
+              className={'hide-nav-on-xs'}
+            >
+              <Menu.Item key="menu-search">
+                <Search
+                  placeholder="键入进行搜索"
+                  onSearch={value => /*console.log(value)*/ value}
+                  style={{ width: 200 }}
+                />
+              </Menu.Item>
 
-            <SubMenu
+              <SubMenu
                 title={
                   <span className="submenu-title-wrapper">
                     <Icon type="edit" />
@@ -93,33 +93,32 @@ class Navbar extends React.Component {
                     <Icon type="caret-down" />
                   </span>
                 }
-            >
-              {navbarSubData.items.map(item => (
-                <Menu.Item key={item.link}>
-                   <Link to={item.link}>
-                     {item.title}
-                   </Link>
+              >
+                {navbarSubData.items.map(item => (
+                  <Menu.Item key={item.link}>
+                    <Link to={item.link}>
+                      {item.title}
+                    </Link>
+                  </Menu.Item>
+                ))}
+              </SubMenu>
+
+              {navbarData.items.map(item => (
+                <Menu.Item key={item.link}
+                  className={classNames({
+                    'link': true,
+                  })}
+                >
+                  <Link to={item.link} >
+                    {item.title}
+                  </Link>
                 </Menu.Item>
               ))}
-            </SubMenu>
-
-            {navbarData.items.map(item => (
-              <Menu.Item key={item.link}
-               className={classNames({
-                 'link' : true,
-                 'layui-hide-xs' : true,
-               })}
-              >
-                <Link to={item.link} >
-                  {item.title}
-                </Link>
-              </Menu.Item>
-            ))}
-          </Menu>
-        </ul>
-      </div>
-    </nav>
-  )
+            </Menu>
+          </ul>
+        </div>
+      </nav>
+    )
   }
 }
 
