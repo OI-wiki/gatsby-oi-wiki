ba989c5b3400a0dc885a2a280a302e52e8910fcc
Ir1d <sirius.caffrey@gmail.com>
2020-03-14 19:29:36 +0800

    style: npm run format

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -5,7 +5,7 @@ import { Card } from "antd"
 import { ChipSet, Chip } from "@material/react-chips"
 import AuthorsArray from "./AuthorsArray"
 import Tags from "./Tags"
-import { MdBuild, MdCopyright, MdEdit } from "react-icons/md";
+import { MdBuild, MdCopyright, MdEdit } from "react-icons/md"
 import theme from "../theme"
 
 function Header({ num }) {
@@ -15,14 +15,46 @@ function Header({ num }) {
 }
 function Meta({ authors, tags }) {
   return (
-    <Card sx={{lineHeights: theme.lineHeights.body}}>
+    <Card sx={{ lineHeights: theme.lineHeights.body }}>
       <AuthorsArray authors={authors} />
       <Tags tags={tags} />
-      <div sx={{'border-left': '5px solid #ff1700', 'margin': '20px 0',
-  'padding-left': '1.5rem'}}>
-        <span><MdBuild />本页面最近更新：</span><span class="facts_modified"></span>，<a class="edit_history">更新历史</a><br />
-      <span><MdEdit />发现错误？想一起完善？ <a href="{{ page.edit_url }}" title="{{ lang.t('edit.link.title') }}" class="page_edit_url">在 GitHub 上编辑此页！</a></span><br />
-      <span><MdCopyright />本页面的全部内容在 <strong><a href="https://creativecommons.org/licenses/by-sa/4.0/deed.zh">CC BY-SA 4.0</a> 和 <a href="https://github.com/zTrix/sata-license">SATA</a></strong> 协议之条款下提供，附加条款亦可能应用</span>
+      <div
+        sx={{
+          "border-left": "5px solid #ff1700",
+          margin: "20px 0",
+          "padding-left": "1.5rem",
+        }}
+      >
+        <span>
+          <MdBuild />
+          本页面最近更新：
+        </span>
+        <span class="facts_modified"></span>，
+        <a class="edit_history">更新历史</a>
+        <br />
+        <span>
+          <MdEdit />
+          发现错误？想一起完善？{" "}
+          <a
+            href="{{ page.edit_url }}"
+            title="{{ lang.t('edit.link.title') }}"
+            class="page_edit_url"
+          >
+            在 GitHub 上编辑此页！
+          </a>
+        </span>
+        <br />
+        <span>
+          <MdCopyright />
+          本页面的全部内容在{" "}
+          <strong>
+            <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.zh">
+              CC BY-SA 4.0
+            </a>{" "}
+            和 <a href="https://github.com/zTrix/sata-license">SATA</a>
+          </strong>{" "}
+          协议之条款下提供，附加条款亦可能应用
+        </span>
       </div>
     </Card>
   )
