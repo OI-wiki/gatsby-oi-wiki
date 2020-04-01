1b8e4e5a765ada298508593ece48fa8df0749c12
Margatroid <i@margatroid.xyz>
2020-04-01 22:45:58 +0800

    chore: refactor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { graphql, StaticQuery } from "gatsby"
 import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
 import { jsx } from "theme-ui"
 
 export default (props) => (
@@ -24,10 +25,14 @@ function FooterContent({ data }) {
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
-        Copyright © 2016 - {date.substr(0, 4)} OI Wiki Team
+        <Typography>
+          Copyright © 2016 - {date.substr(0, 4)} OI Wiki Team
+        </Typography>
       </Grid>
       <Grid item xs={12}>
-        最近更新: {hash.substr(0, 7)}, {date.substr(0, 10)}
+        <Typography>
+          最近更新: {hash.substr(0, 7)}, {date.substr(0, 10)}
+        </Typography>
       </Grid>
     </Grid>
   )
