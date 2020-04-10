fcf08a77a46da1c6c0ef05002b31f2ad994414f0
frank-xjh <frank99-xu@outlook.com>
2020-04-10 21:40:00 +0800

    fix(gatsby-remark-autolink-headers): concealed anchor

--- plugins/gatsby-local-autolink-headers/index.js
+++ plugins/gatsby-local-autolink-headers/index.js
@@ -11,18 +11,23 @@ function patch(context, key, value) {
   return context[key]
 }
 
+function concealedAnchor(id) {
+  const nodeHtml = `<a class="anchor" id="${id}"></a>`
+
+  return nodeHtml
+}
+
 const svgIcon = `<svg aria-hidden="true" focusable="false" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`
 
 module.exports = ({
   markdownAST
 }, {
   icon = svgIcon,
-  className = `anchor`,
+  className = `anchorLink`,
   maintainCase = false,
   removeAccents = false,
   enableCustomId = false,
   isIconAfterHeader = false,
-  offsetY = 0,
 }) => {
   slugs.reset()
 
@@ -47,14 +52,11 @@ module.exports = ({
       const slug = slugs.slug(toString(node), maintainCase)
       id = removeAccents ? deburr(slug) : slug
     }
-    const data = patch(node, `data`, {})
 
-    patch(data, `id`, id)
-    patch(data, `htmlAttributes`, {})
-    patch(data, `hProperties`, {})
-    patch(data.htmlAttributes, `id`, id)
-    patch(data.hProperties, `id`, id)
-    patch(data.hProperties, `style`, `position:relative;`)
+    node.children.push({
+      type: 'html',
+      value: concealedAnchor(id),
+    })
 
     if (icon !== false) {
       const label = id.split(`-`).join(` `)
@@ -77,75 +79,13 @@ module.exports = ({
         },
       })
     }
+
   })
-
-  const styles = `
-  <style type="text/css">
-    .${className}.before {
-      position: absolute;
-      top: 0;
-      left: 0;
-      transform: translateX(-100%);
-      padding-right: 4px;
-    }
-    .${className}.after {
-      display: inline-block;
-      padding-left: 4px;
-    }
-    h1 .${className} svg,
-    h2 .${className} svg,
-    h3 .${className} svg,
-    h4 .${className} svg,
-    h5 .${className} svg,
-    h6 .${className} svg {
-      visibility: hidden;
-    }
-    h1:hover .${className} svg,
-    h2:hover .${className} svg,
-    h3:hover .${className} svg,
-    h4:hover .${className} svg,
-    h5:hover .${className} svg,
-    h6:hover .${className} svg,
-    h1 .${className}:focus svg,
-    h2 .${className}:focus svg,
-    h3 .${className}:focus svg,
-    h4 .${className}:focus svg,
-    h5 .${className}:focus svg,
-    h6 .${className}:focus svg {
-      visibility: visible;
-    }
-</style>
-  `
-
-  // This script used to have `let scrollTop` and `let clientTop` instead of
-  // current ones which are `var`. It is changed due to incompatibility with
-  // older browsers (that do not implement `let`). See:
-  //  - https://github.com/gatsbyjs/gatsby/issues/21058
-  //  - https://github.com/gatsbyjs/gatsby/pull/21083
-  const script = `
-  <script>
-    document.addEventListener("DOMContentLoaded", function(event) {
-      var hash = window.decodeURI(location.hash.replace('#', ''))
-      if (hash !== '') {
-        var element = document.getElementById(hash)
-        if (element) {
-          var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-          var clientTop = document.documentElement.clientTop || document.body.clientTop || 0
-          var offset = element.getBoundingClientRect().top + scrollTop - clientTop
-          // Wait for the browser to finish rendering before scrolling.
-          setTimeout((function() {
-            window.scrollTo(0, offset - ${offsetY})
-          }), 0)
-        }
-      }
-    })
-    </script>
-    `
   
   /**
   markdownAST.children.push({
     type: 'html',
-    value: styles + script
+    value: renderHtml(className, offsetY)
   });
   */
 
