7458907ec509b8e0b1710de3917b311296a9fbee
frank-xjh <frank99-xu@outlook.com>
2020-04-10 23:26:54 +0800

    fix: anchor css

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default function HTML(props) {
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
       {props.headComponents}
+      <link rel="stylesheet" type="text/css" href="/extra.css" />
     </head>
     <body {...props.bodyAttributes}>
     {props.preBodyComponents}
