cf0857c913e57876483e33ea9beae88c3b1b057a
Margatroid <i@margatroid.xyz>
2020-04-10 21:12:14 +0800

    fix: add key prop and fix style

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,12 +1,18 @@
-import { graphql } from "gatsby"
+/** @jsx jsx */
+import Chip from "@material-ui/core/Chip"
 import { makeStyles } from "@material-ui/core/styles"
+import { graphql } from "gatsby"
 import kebabCase from "lodash/kebabCase"
 import PropTypes from "prop-types"
 import React from "react"
+import { jsx } from "theme-ui"
 import Layout from "../components/Layout"
-import Link from "../components/Link"
-import Chip from '@material-ui/core/Chip'
 
+const useStyles = makeStyles((theme) => ({
+  chip: {
+    margin: theme.spacing(0.5),
+  },
+}))
 const TagsPage = ({
                     data: {
                       allMdx: { group },
@@ -15,23 +21,26 @@ const TagsPage = ({
                       },
                     },
                     location,
-                  }) => (
-
-  <Layout location={location} noMeta="true" title="标签页">
-    <div>
-      {group.map((tag) => (
-        <Chip
-          label={`${tag.fieldValue}(${tag.totalCount})`}
-          variant="outlined"
-          component={"a"}
-          clickable
-          href={"/tags/" + kebabCase(tag.fieldValue)}
-          style={{margin: 4}}
-        />
-      ))}
-    </div>
-  </Layout>
-)
+                  }) => {
+  const classes = useStyles()
+  return (
+    <Layout location={location} noMeta="true" title="标签页">
+      <div>
+        {group.map((tag) => (
+          <Chip
+            label={`${tag.fieldValue}(${tag.totalCount})`}
+            variant="outlined"
+            component={"a"}
+            clickable
+            key={tag.fieldValue}
+            href={"/tags/" + kebabCase(tag.fieldValue)}
+            className={classes.chip}
+          />
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
 TagsPage.propTypes = {
   data: PropTypes.shape({
