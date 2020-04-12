1d60d3bab77638cfad020f62bb40071025fa1381
Ir1d <sirius.caffrey@gmail.com>
2020-04-12 13:04:03 +0800

    make it wider for small screen

--- src/components/Search.js
+++ src/components/Search.js
@@ -50,7 +50,7 @@ const styles = (theme) => ({
     transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("md")]: {
-      width: "12vw",
+      width: "15vw",
       "&:focus": {
         width: "30vw",
       },
