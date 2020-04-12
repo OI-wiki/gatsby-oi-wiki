75fedf6e307535b2f7f3570d3b58969515a86099
Xiaodai Dai <diauweb@live.com>
2020-04-12 19:50:10 +0800

    feat(detail): stylish

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,14 +1,20 @@
 /** @jsx jsx */
 import { MdEdit, MdExpandMore } from "react-icons/md"
 import { jsx } from "theme-ui"
-import { ExpansionPanelSummary, makeStyles } from '@material-ui/core'
+import { ExpansionPanelSummary } from '@material-ui/core'
+import { makeStyles } from "@material-ui/core/styles"
+import blue from '@material-ui/core/colors/blue'
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    background: blue[50]
+  },
   content: {
     margin: "0",
     "& p": {
       margin: "0"
-    }
+    },
+    fontWeight: "bold"
   },
 }))
 
@@ -22,58 +28,10 @@ export default function({ className = null, children, ...props }) {
     aria-controls="expand"
     {...props}
   >
+    <MdEdit style={{ 
+      margin: "0px 10px 2px -5px", 
+      fontSize: "20px", 
+      color: blue[500] }}/>
     {children}
   </ExpansionPanelSummary>
-  // if (className.match("note")) {
-  //   return (
-  //     <summary
-  //       className={className}
-  //       {...props}
-  //       sx={{
-  //         margin: "0 -0.6rem",
-  //         padding: ".2rem .6rem .2rem 1rem",
-  //         borderBottom: ".05rem solid rgba(68,138,255,.1)",
-  //         backgroundColor: "rgba(68,138,255,.1)",
-  //         fontWeight: 700,
-  //         outline: "none",
-  //         cursor: "pointer",
-  //         p: {
-  //           display: "inline-block",
-  //           margin: 0,
-  //           fontSize: "14px",
-  //         },
-  //         svg: {
-  //           fontSize: "20px",
-  //         },
-  //         listStyle: "none",
-  //         "::-webkit-details-marker": {
-  //           display: "none",
-  //         },
-  //       }}
-  //     >
-  //       <MdEdit
-  //         sx={{
-  //           verticalAlign: "-4px",
-  //           marginTop: "7px",
-  //           mr: "0.6rem",
-  //         }}
-  //       />
-  //       {children}
-  //       <MdExpandMore
-  //         className="expand-more-icon"
-  //         sx={{
-  //           verticalAlign: "-4px",
-  //           marginTop: "7px",
-  //           ml: "1rem",
-  //           float: "right",
-  //         }}
-  //       />
-  //     </summary>
-  //   )
-  // }
-  // return (
-  //   <summary className={className} {...props}>
-  //     {children}
-  //   </summary>
-  // )
 }
