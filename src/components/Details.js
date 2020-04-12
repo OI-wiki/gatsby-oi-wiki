cc34253e8595f7266b915ec27fcf648cb0f33bb6
Xiaodai Dai <diauweb@live.com>
2020-04-12 19:50:10 +0800

    feat(detail): stylish

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,32 +1,16 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
 import { ExpansionPanel, ExpansionPanelDetails, Container } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core"
-
-const detailsStyle = {
-  boxShadow:
-    "0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12), 0 3px 1px -2px rgba(0,0,0,.2)",
-  position: "relative",
-  margin: "1.5625em 0",
-  padding: "0 .6rem",
-  borderLeft: ".25rem solid #448aff",
-  borderRadius: ".1rem",
-  overflow: "auto",
-  fontSize: "14px",
-  "&[open]": {
-    summary: {
-      ".expand-more-icon": {
-        transform: "rotate(180deg)",
-      },
-    },
-  },
-}
+import { makeStyles } from "@material-ui/core/styles"
+import blue from '@material-ui/core/colors/blue'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     "&, &:first-child, &:last-child": {
       margin: "0.8em 0 !important"
     },
+    borderLeft: ".3rem solid",
+    borderLeftColor: blue[500]
   },
   "expanded": {
     "&, &:first-child, &:last-child": {
@@ -41,20 +25,11 @@ export default function({ className = "", children, ...props }) {
   
   return <ExpansionPanel variant="outlined" classes={classes} defaultExpanded={!!className.match("open")}>
     {children[0]}
-    <ExpansionPanelDetails>
+    <ExpansionPanelDetails style={{padding: "0"}}>
       <Container>
         {children.slice(1)}
       </Container>
     </ExpansionPanelDetails>
   </ExpansionPanel>
-
-  // return className.match("open") ? (
-  //   <details className={className} sx={detailsStyle} {...props} open>
-  //     {children}
-  //   </details>
-  // ) : (
-  //   <details className={className} sx={detailsStyle} {...props}>
-  //     {children}
-  //   </details>
-  // )
+  
 }
