0c9766ea70d82d4e6e9f90a35002fbd80a1a91bd
CBW2007 <2337118038@qq.com>
2020-04-24 08:51:08 +0800

    feat(meta): 为meta中的开始编辑增加编辑前须知

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -4,11 +4,14 @@ import { makeStyles } from '@material-ui/core/styles'
 import CopyrightIcon from '@material-ui/icons/Copyright'
 import EditIcon from '@material-ui/icons/Edit'
 import HistoryIcon from '@material-ui/icons/History'
-import React from 'react'
+import React, { useState } from 'react'
 
 import AuthorsArray from './AuthorsArray'
 import Link from './Link'
 import Tags from './Tags'
+import EditWarn from './EditWarn'
+
+import lightBlue from '@material-ui/core/colors/lightBlue'
 
 const useStyles = makeStyles((theme) => ({
   metaicon: {
@@ -26,47 +29,60 @@ const useStyles = makeStyles((theme) => ({
     paddingLeft: '.5rem',
     textDecoration: 'none',
   },
+  link: {
+    color: lightBlue[500],
+    textDecoration: 'none',
+    ':hover': {
+      textDecoration: 'none',
+    },
+    '&.active': {
+      color: theme.palette.text.primary,
+    },
+  },
 }))
 
 function Meta ({ authors, tags, relativePath, modifiedTime, noMeta }) {
-  const editURL = 'https://github.com/OI-wiki/OI-wiki/edit/master/docs/'
   const historyURL = 'https://github.com/OI-wiki/OI-wiki/commits/master/docs/'
   const classes = useStyles()
+  const [dialogOpen, setDialogOpen] = useState(false)
   if (noMeta === 'false') {
     return (
-      <Paper className={classes.paper} variant="outlined">
-        <AuthorsArray authors={authors} />
-        <Divider className={classes.divider} />
-        <Tags tags={tags} />
-        <div className={classes.meta}>
-          <span>
-            <HistoryIcon fontSize="small" className={classes.metaicon} />
-            本页面最近更新：
-          </span>
-          <span>{modifiedTime}</span>，
-          <Link href={historyURL + relativePath}>更新历史</Link>
-          <br />
-          <span>
-            <EditIcon fontSize="small" className={classes.metaicon} />
-            发现错误？想一起完善？{' '}
-            <Link href={editURL + relativePath} title="编辑此页">
-              在 GitHub 上编辑此页！
-            </Link>
-          </span>
-          <br />
-          <span>
-            <CopyrightIcon fontSize="small" className={classes.metaicon} />
-            本页面的全部内容在{' '}
-            <strong>
-              <Link href="https://creativecommons.org/licenses/by-sa/4.0/deed.zh">
-                CC BY-SA 4.0
-              </Link>{' '}
-              和 <Link href="https://github.com/zTrix/sata-license">SATA</Link>
-            </strong>{' '}
-            协议之条款下提供，附加条款亦可能应用
-          </span>
-        </div>
-      </Paper>
+      <>
+        <EditWarn relativePath={relativePath} dialogOpen={dialogOpen} setDialogOpen={setDialogOpen} />
+        <Paper className={classes.paper} variant="outlined">
+          <AuthorsArray authors={authors} />
+          <Divider className={classes.divider} />
+          <Tags tags={tags} />
+          <div className={classes.meta}>
+            <span>
+              <HistoryIcon fontSize="small" className={classes.metaicon} />
+              本页面最近更新：
+            </span>
+            <span>{modifiedTime}</span>，
+            <Link href={historyURL + relativePath}>更新历史</Link>
+            <br />
+            <span>
+              <EditIcon fontSize="small" className={classes.metaicon} />
+              发现错误？想一起完善？{' '}
+              <a href="#" onClick={() => setDialogOpen(true)} className={classes.link}>
+                在 GitHub 上编辑此页！
+              </a>
+            </span>
+            <br />
+            <span>
+              <CopyrightIcon fontSize="small" className={classes.metaicon} />
+              本页面的全部内容在{' '}
+              <strong>
+                <Link href="https://creativecommons.org/licenses/by-sa/4.0/deed.zh">
+                  CC BY-SA 4.0
+                </Link>{' '}
+                和 <Link href="https://github.com/zTrix/sata-license">SATA</Link>
+              </strong>{' '}
+              协议之条款下提供，附加条款亦可能应用
+            </span>
+          </div>
+        </Paper>
+      </>
     )
   } else return <div />
 }
