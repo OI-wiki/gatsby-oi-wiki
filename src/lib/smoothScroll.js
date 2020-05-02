54d4b53ba21d3dc0ef032175dc4f23ec8a8bbf83
Margatroid <i@margatroid.xyz>
2020-05-02 17:50:02 +0800

    fix: fix scroll issues

--- src/lib/smoothScroll.js
+++ src/lib/smoothScroll.js
@@ -1,16 +1,22 @@
 /**
  * 获取动画进行 progress 时，物体的位移
- * 该动画曲线是阻尼振动模型。
+ * 该动画曲线是阻尼振动模型。弹簧的劲度系数是 500，阻力系数是 100，物体质量为 3
  * @param progress - 动画的播放进度，[0, 1] 之间的实数
  * @returns - 返回物体的位移百分比，[0, 1] 之间的实数
+ * 动画曲线的方程为 f(t) = 1.00283 * (
+ * \frac{1}{4} \, {\left(\sqrt{10} - 2\right)} e^{\left(-\frac{10}{3} \, x {\left(\sqrt{10} + 5\right)}\right)} -
+ * \frac{1}{4} \, {\left(\sqrt{10} + 2\right)} e^{\left(\frac{10}{3} \, x {\left(\sqrt{10} - 5\right)}\right)} + 1
+ * )
  * */
 function getDisplacement (progress) {
-  const t = progress
-  if (t < 0) { return 0 }
-  // FIXME: 可能是由于曲线的设计问题，在 t=1 的时候没法达到目的位置
-  return 0.0454545 * Math.exp(-701.297338 * t) *
-    (0.18034 - 22.1803 * Math.exp(695.59609 * t) +
-      22 * Math.exp(701.297338 * t))
+  if (progress < 0) {
+    return 0
+  }
+  const f = (t) => 1 + 0.25 * (
+    1.1622776601683795 * Math.exp((-10 / 3) * t * 8.16227766016838) -
+    5.16227766016838 * Math.exp((10 / 3) * t * -1.8377223398316205)
+  )
+  return f(progress) * 1.00283
 }
 
 /**
@@ -19,17 +25,15 @@ function getDisplacement (progress) {
  * @param duration 移动动画的持续时间，用毫秒表示
  */
 function smoothScrollTo (yCoordinate, duration = 1000) {
-  const offset = yCoordinate - document.documentElement.scrollTop
+  const offset = yCoordinate - window.scrollY
   const startTime = performance.now()
-  let totalDisplacement = 0
+  const startPosition = window.scrollY
   const performAnimation = (time) => {
     if (time - startTime > duration) {
       return
     }
-    const displacement = getDisplacement((time - startTime) / duration)
-    const delta = (displacement - totalDisplacement) * offset
-    totalDisplacement = displacement
-    window.scrollBy(0, delta)
+    const displacement = offset * getDisplacement((time - startTime) / duration)
+    window.scrollTo(0, startPosition + displacement)
     requestAnimationFrame(performAnimation)
   }
   requestAnimationFrame(performAnimation)
