d8e426aac2a59429e48a53011866c7cd9ac49891
Xiaodai Dai <diauweb@live.com>
2020-05-04 21:54:26 +0800

    feat: add global css override

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,29 @@
 import { createMuiTheme } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
 
 const theme = createMuiTheme({
   palette: {
-    type: 'dark',
   },
 })
 
-export default theme
+const globalStyles = withStyles((theme) => ({
+  '@global': {
+    a: {
+      color: theme.palette.primary.main,
+      textDecoration: 'none',
+      ':hover': {
+        textDecoration: 'none',
+      },
+      '&.active, &:active': {
+        color: theme.palette.primary.light,
+      },
+    },
+  },
+}))
+
+function CustomCssEl () { return null }
+CustomCssEl.propTypes = { classes: PropTypes.object.isRequired }
+
+export const CustomCssBaseline = globalStyles(CustomCssEl)
+export { theme }
