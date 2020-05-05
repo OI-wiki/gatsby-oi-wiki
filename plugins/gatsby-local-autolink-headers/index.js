02fe3c64a80ebdda1775bc903f250f6b45fcc603
CBW2007 <2337118038@qq.com>
2020-05-05 19:54:25 +0800

    style: format codes with eslint

--- plugins/gatsby-local-autolink-headers/index.js
+++ plugins/gatsby-local-autolink-headers/index.js
@@ -1,31 +1,31 @@
-const toString = require(`mdast-util-to-string`)
-const visit = require(`unist-util-visit`)
-const slugs = require(`github-slugger`)()
-const deburr = require(`lodash/deburr`)
+const toString = require('mdast-util-to-string')
+const visit = require('unist-util-visit')
+const slugs = require('github-slugger')()
+const deburr = require('lodash/deburr')
 
-function concealedAnchor(conClassName, id) {
+function concealedAnchor (conClassName, id) {
   const nodeHtml = `<a class="${conClassName}" id="${id}"></a>`
 
   return nodeHtml
 }
 
-const svgIcon = `<svg aria-hidden="true" focusable="false" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`
+const svgIcon = '<svg aria-hidden="true" focusable="false" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>'
 
 module.exports = (
   { markdownAST },
   {
     icon = svgIcon,
-    className = "anchorLink",
-    conClassName = "anchor",
+    className = 'anchorLink',
+    conClassName = 'anchor',
     maintainCase = false,
     removeAccents = false,
     enableCustomId = false,
     isIconAfterHeader = false,
-  }
+  },
 ) => {
   slugs.reset()
 
-  visit(markdownAST, `heading`, (node) => {
+  visit(markdownAST, 'heading', (node) => {
     let id
     if (enableCustomId && node.children.length > 0) {
       const last = node.children[node.children.length - 1]
@@ -48,26 +48,26 @@ module.exports = (
     }
 
     node.children.push({
-      type: "html",
+      type: 'html',
       value: concealedAnchor(conClassName, id),
     })
 
     if (icon !== false) {
-      const label = id.split(`-`).join(` `)
-      const method = isIconAfterHeader ? `push` : `unshift`
+      const label = id.split('-').join(' ')
+      const method = isIconAfterHeader ? 'push' : 'unshift'
       node.children[method]({
-        type: `link`,
+        type: 'link',
         url: `#${id}`,
         title: null,
         children: [],
         data: {
           hProperties: {
-            "aria-label": `${label} permalink`,
-            class: `${className} ${isIconAfterHeader ? `after` : `before`}`,
+            'aria-label': `${label} permalink`,
+            class: `${className} ${isIconAfterHeader ? 'after' : 'before'}`,
           },
           hChildren: [
             {
-              type: `raw`,
+              type: 'raw',
               // The Octicon link icon is the default. But users can set their own icon via the "icon" option.
               value: icon,
             },
@@ -78,7 +78,7 @@ module.exports = (
   })
 
   markdownAST.children.push({
-    type: "html",
+    type: 'html',
     value: `<style>.${className}.before{position:absolute;top:0;left:0;transform:translateX(-100%);padding-right:4px}.${className}.after{display:inline-block;padding-left:4px}h1 .${className} svg,h2 .${className} svg,h3 .${className} svg,h4 .${className} svg,h5 .${className} svg,h6 .${className} svg{visibility:hidden}h1 .${className}:focus svg,h1:hover .${className} svg,h2 .${className}:focus svg,h2:hover .${className} svg,h3 .${className}:focus svg,h3:hover .${className} svg,h4 .${className}:focus svg,h4:hover .${className} svg,h5 .${className}:focus svg,h5:hover .${className} svg,h6 .${className}:focus svg,h6:hover .${className} svg{visibility:visible}a.anchor{display:inline-block;position:relative;top:-159px;visibility:hidden}@media only screen and (max-width:1279.95px){a.anchor{top:-101px}}</style>`,
   })
 
