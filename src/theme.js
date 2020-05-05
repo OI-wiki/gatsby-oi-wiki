cd820afea282eb8b7bfaab3784e8821b39a40661
Ir1d <sirius.caffrey@gmail.com>
2020-05-05 22:31:51 +0800

    improvement: override global style of blockquote and inline code

--- src/theme.js
+++ src/theme.js
@@ -25,6 +25,19 @@ const globalStyles = withStyles((theme) => ({
         color: theme.palette.primary.light,
       },
     },
+    blockquote: {
+      paddingLeft: '1em',
+      margin: '1em 3em 1em 2em',
+      borderLeft: '4px solid rgba(0,0,0,.12)',
+    },
+    code: {
+      padding: '0.5em 0',
+      backgroundColor: 'hsla(0,0%,92.5%,.5)',
+    },
+    'pre code': {
+      padding: '0.2em 0',
+      backgroundColor: '#1E1E1E',
+    },
   },
 }))
 
