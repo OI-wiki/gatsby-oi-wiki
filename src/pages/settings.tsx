c2a8df94ed5fb0027995cdbc701acf01028791fa
Margatroid <i@margatroid.xyz>
2020-05-06 14:06:16 +0800

    feat: add darkmode switch

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,26 +1,17 @@
 import Grid from '@material-ui/core/Grid'
 import React from 'react'
 import Layout from '../components/Layout'
-import useLocalStorage from '../lib/useLocalstorage'
-import Settings from '../types/settings'
+import createPersistedState from 'use-persisted-state'
 import InputLabel from '@material-ui/core/InputLabel'
 import MenuItem from '@material-ui/core/MenuItem'
-// import FormHelperText from '@material-ui/core/FormHelperText'
 import FormControl from '@material-ui/core/FormControl'
 import Select from '@material-ui/core/Select'
+import defaultSettings from '../lib/defaultSettings'
+const useConfig = createPersistedState('settings')
 
 function SettingsPage (props: {location: string}): unknown {
   const { location } = props
-  const defaultSettings: Settings = {
-    darkMode: {
-      type: 'user-preference',
-      brightness: 0.7,
-    },
-    animation: {
-      enable: true,
-    },
-  }
-  const [settings, setSettings] = useLocalStorage('settings', defaultSettings)
+  const [settings, setSettings] = useConfig(defaultSettings)
   const updateSetting = (newSettings): void => (setSettings({ ...defaultSettings, ...newSettings }))
   return (
     // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
@@ -39,6 +30,7 @@ function SettingsPage (props: {location: string}): unknown {
             <Select
               value={settings.darkMode.type}
               onChange={(e) => {
+                console.log(e.target.value)
                 updateSetting({
                   darkMode: {
                     type: e.target.value,
@@ -49,7 +41,7 @@ function SettingsPage (props: {location: string}): unknown {
               <MenuItem value={'user-preference'}>跟随系统</MenuItem>
               <MenuItem value={'scheduled'}>定时开启</MenuItem>
               <MenuItem value={'always-on'}>总是打开</MenuItem>
-              <MenuItem value={'always-disable'}>总是关闭</MenuItem>
+              <MenuItem value={'always-off'}>总是关闭</MenuItem>
             </Select>
           </FormControl>
         </Grid>
