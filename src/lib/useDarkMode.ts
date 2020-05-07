1a651f0d8fb2974bacd662346f50c0fc2e1dafa0
Margatroid <i@margatroid.xyz>
2020-05-07 08:08:49 +0800

    feat: add useDarkMode hook

---
+++ src/lib/useDarkMode.ts
@@ -0,0 +1,21 @@
+import createPersistedState from 'use-persisted-state'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import defaultSettings from './defaultSettings'
+
+const useConfig = createPersistedState('settings')
+
+export default function useDarkMode (): boolean {
+  const [setting] = useConfig(defaultSettings)
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  let enableDark: boolean
+  if (setting.darkMode.type === 'always-on') {
+    enableDark = true
+  } else if (setting.darkMode.type === 'always-off') {
+    enableDark = false
+  } else if (setting.darkMode.type === 'user-preference') {
+    enableDark = prefersDarkMode
+  } else {
+    enableDark = prefersDarkMode // TODO: to be implemented
+  }
+  return enableDark
+}
