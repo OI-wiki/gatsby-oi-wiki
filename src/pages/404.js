b1c55ce49b9f30d3d6790e53ae9c6121226a95a2
Xiaodai Dai <diauweb@live.com>
2020-05-10 21:42:19 +0800

    feat: enable comment on 404 pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,13 @@ import React from 'react'
 import Layout from '../components/Layout'
 
 const page = (location) => (
-  <Layout location={location} noEdit="true" title="404 Not Found">
+  <Layout location={location} noEdit="true" title="404 Not Found" noComment="false">
     <div>
       <div>
         <h1>没有找到这个页面</h1>
         <p>如果你是无意间来到这里的，请告诉我们你是从哪里来的哦。</p>
+        <br/>
+        <br/>
       </div>
     </div>
   </Layout>
