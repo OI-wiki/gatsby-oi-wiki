eaab42d1c4a2f43ed00f93de6306afbe13dae945
Margatroid <i@margatroid.xyz>
2020-05-26 09:35:23 +0800

    chore: bump eslint to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
