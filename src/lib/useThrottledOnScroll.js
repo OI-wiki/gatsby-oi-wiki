0b87338113fdb803083ed9446b4a5a1bbcfe76ab
Margatroid <i@margatroid.xyz>
2020-06-11 13:27:34 +0800

    fix: fix throttle

--- src/lib/useThrottledOnScroll.js
+++ src/lib/useThrottledOnScroll.js
@@ -1,18 +1,46 @@
 import { useEffect, useMemo } from 'react'
-
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 const noop = () => {}
 
-// https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_throttle
-function throttle (func, timeFrame) {
-  let lastTime = 0
-  return () => {
-    const now = new Date()
-    if (now - lastTime >= timeFrame) {
-      func()
-      lastTime = now
-    }
+// from underscore.js
+function throttle (func, wait, options) {
+  let timeout, context, args, result
+  let previous = 0
+  if (!options) options = {}
+
+  const later = function () {
+    previous = options.leading === false ? 0 : Date.now()
+    timeout = null
+    result = func.apply(context, args)
+    if (!timeout) context = args = null
   }
+
+  const throttled = function () {
+    const now = Date.now()
+    if (!previous && options.leading === false) previous = now
+    const remaining = wait - (now - previous)
+    context = this
+    args = arguments
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+      }
+      previous = now
+      result = func.apply(context, args)
+      if (!timeout) context = args = null
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining)
+    }
+    return result
+  }
+
+  throttled.cancel = function () {
+    clearTimeout(timeout)
+    previous = 0
+    timeout = context = args = null
+  }
+  return throttled
 }
 
 export default function useThrottledOnScroll (callback, delay) {
