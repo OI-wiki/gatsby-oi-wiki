485a9c8841b473e464f75fe8c5413f837fd59049
Margatroid <i@margatroid.xyz>
2020-07-21 22:20:14 +0800

    fix(comment-system): fix button position

--- src/components/commentSystem/CommentInput.tsx
+++ src/components/commentSystem/CommentInput.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { makeStyles, Avatar, TextField, Grid } from '@material-ui/core'
+import { makeStyles, Avatar, TextField, Grid, Button, Hidden } from '@material-ui/core'
 
 interface Props {
   avatarLink: string,
@@ -21,6 +21,13 @@ const useStyles = makeStyles(theme => ({
   },
   commentMargin: {
     marginTop: theme.spacing(1),
+  },
+  button: {
+    marginBottom: theme.spacing(1),
+    marginTop: theme.spacing(1),
+  },
+  buttonDiv: {
+    textAlign: 'right',
     marginBottom: theme.spacing(1),
   },
 }))
@@ -29,22 +36,29 @@ const CommentInput: React.FC<Props> = (props) => {
   const classes = useStyles()
   const [content, setContent] = useState('')
   return (
-    <Grid container className={classes.commentMargin} justify="space-around" spacing={2}>
-      <Grid item >
-        <Avatar alt={props.name} src={props.avatarLink}/>
+    <>
+      <Grid container className={classes.commentMargin} justify="space-around" spacing={2}>
+        <Hidden smDown>
+          <Grid item>
+            <Avatar alt={props.name} src={props.avatarLink}/>
+          </Grid>
+        </Hidden>
+        <Grid item xs>
+          <TextField
+            placeholder="我们鼓励在讨论区讨论有意义的内容及关于文章的勘误，无意义的讨论将会被管理员删除"
+            multiline
+            fullWidth
+            rows={5}
+            value={content}
+            onChange={(e) => { setContent(e.target.value) }}
+            variant="outlined"
+          />
+        </Grid>
       </Grid>
-      <Grid item xs>
-        <TextField
-          placeholder="我们鼓励在讨论区讨论有意义的内容及关于文章的勘误，无意义的讨论将会被管理员删除"
-          multiline
-          fullWidth
-          rows={5}
-          value={content}
-          onChange={(e) => { setContent(e.target.value) }}
-          variant="outlined"
-        />
-      </Grid>
-    </Grid>
+      <div className={classes.buttonDiv}>
+        <Button variant="contained" color="primary" className={classes.button} size="small">评论</Button>
+      </div>
+    </>
   )
 }
 
