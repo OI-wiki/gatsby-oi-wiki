313dba9a409ab4d85085ac6136bdc60957dee8e3
Margatroid <i@margatroid.xyz>
2020-08-03 18:31:42 +0800

    feat: add prism

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import './static/extra.css'
-
+import './static/prism-theme.css'
+// import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 export const onRouteUpdate = () => {
   if (process.env.GATSBY_IS_DEV) {
     // eslint-disable-next-line no-undef
