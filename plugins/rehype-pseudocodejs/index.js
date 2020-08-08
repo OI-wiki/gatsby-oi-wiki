728b4a87d6a2ba31bdf01dc3ee8090cf8b8ef97c
Xiaodai Dai <diauweb@live.com>
2020-08-08 21:26:36 +0800

    feat: use local css and mathjax math fonts

--- plugins/rehype-pseudocodejs/index.js
+++ plugins/rehype-pseudocodejs/index.js
@@ -8,10 +8,7 @@ const unified = require('unified')
 const parse = require('rehype-parse')
 const pseudo = require('./pcr')
 
-module.exports = (options) => {
-  options = options || {}
-
-  let finish = false
+module.exports = () => {
   return (tree) => {
     visit(tree, 'element', visitor)
   }
@@ -26,25 +23,11 @@ module.exports = (options) => {
       const markup = pseudo.renderToString(nodeToString(node))
       const dom = unified()
         .use(parse, { fragment: true })
-        // .use(...options.mathProcessor)
         .parse(markup)
 
       node.children = dom.children
       node.properties = {}
       node.tagName = 'div'
-
-      // if (node.tagName === 'head') {
-      if (!finish) {
-        node.children.push({
-          type: 'element',
-          tagName: 'link',
-          properties: {
-            rel: 'stylesheet',
-            href: 'https://cdn.jsdelivr.net/npm/pseudocode@latest/build/pseudocode.min.css',
-          },
-        })
-        finish = true
-      }
     } catch (e) {
       console.log(e)
     }
