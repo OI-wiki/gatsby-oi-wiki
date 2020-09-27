ab077e461f8ccbf9d4b51872b13d8e4159ea8092
Xiaodai Dai <diauweb@live.com>
2020-09-27 15:17:56 +0800

    feat: themize project structure

--- gatsby-theme-oi-wiki/gatsby-config.js
+++ gatsby-theme-oi-wiki/gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const isProd = process.env.PRODUCTION === 'true' || process.env.RENDER === 'true'
 const condition = (cond, v) => cond ? [v] : []
 if (isProd && process.env.gatsby_executing_command !== 'build') {
@@ -25,7 +27,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: './docs/',
-        path: './docs/',
+        path: path.resolve('./docs/'),
       },
     },
     ...condition(isProd, 'gatsby-plugin-sharp'),
