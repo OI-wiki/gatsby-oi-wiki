9a5a4c76926afa06753f80b44521814ec0c140bd
CBW2007 <2337118038@qq.com>
2020-10-11 21:36:32 +0800

    feat(rate): add rate info

--- gatsby-theme-oi-wiki/src/components/Rate.js
+++ gatsby-theme-oi-wiki/src/components/Rate.js
@@ -3,6 +3,7 @@ import AV from 'leancloud-storage'
 import RcRate from 'rc-rate'
 import 'rc-rate/assets/index.css'
 import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 
 const queryRate = new AV.Query('Rates')
@@ -18,7 +19,6 @@ const useStyles = makeStyles(() => ({
 }))
 
 function change (newValue) {
-  console.log('*')
   var classRate = null
   if (queryCount) {
     classRate = queryData
@@ -59,26 +59,36 @@ function Rate ({ pid }) {
   const classes = useStyles()
   const appId = 'ndFz7SM4piNM35obq15NGD1E-MdYXbMMI'; const appKey = 'UscWyv1U65BHXOx6JUTvOKaD'
   PID = pid
-  const [rateNum, setRateNum] = useState(5)
+  const [rateNum, setRateNum] = useState(-1)
+  const [rateInfo, setRateInfo] = useState('Loading...')
   AV.init({
     appId: appId,
     appKey: appKey,
   })
-  AV.debug.enable()
+  //  AV.debug.enable()
   queryRate.equalTo('pid', pid)
   useEffect(() => {
     const getData = async () => {
       queryCount = await queryRate.count()
       if (queryCount) {
         queryData = await queryRate.first()
-        setRateNum(queryData.get('totalScore') / queryData.get('counts'))
+        const totalScore = queryData.get('totalScore')
+        const counts = queryData.get('counts')
+        const result = Math.round(totalScore / counts * 10) / 10
+        setRateNum(result)
+        setRateInfo(`${result} 分 / ${counts} 票`)
+      } else {
+        setRateNum(0)
+        setRateInfo('-- 分 / 0 票')
       }
     }
     getData()
   }, [])
   return (
     <>
-      <RcRate value={rateNum} onChange={change} className={classes.rateBar} />
+      <RcRate value={rateNum < 0 ? 0 : rateNum} disabled={rateNum < 0} onChange={change} className={classes.rateBar} />
+      <br />
+      <Typography align='center' color='textSecondary' variant='body2' >{rateInfo}</Typography>
       <br />
       <Button onClick={loginOrLogout}>LOGIN/LOGOUT</Button>
     </>
