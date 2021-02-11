bb57a3fef524f90e5f489c4ae0d71d961afb7795
sshwy <jy.cat@qq.com>
2021-02-11 17:43:04 +0800

    override font family

--- gatsby-theme-oi-wiki/src/theme.js
+++ gatsby-theme-oi-wiki/src/theme.js
@@ -214,6 +214,9 @@ const adaptiveTheme = createMuiTheme({
       },
     },
   },
+  typography: {
+    fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif',
+  },
 })
 
 export { adaptiveTheme }
