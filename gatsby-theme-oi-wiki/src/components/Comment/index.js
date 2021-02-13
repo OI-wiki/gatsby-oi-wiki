dfd99ff49ee42070a374d0231d17b394536c1183
sshwy <jy.cat@qq.com>
2021-02-13 10:02:17 +0800

    add icon for comment

--- gatsby-theme-oi-wiki/src/components/Comment/index.js
+++ gatsby-theme-oi-wiki/src/components/Comment/index.js
@@ -1,3 +1,15 @@
+import {
+  Container,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+  makeStyles,
+} from '@material-ui/core'
+import {
+  ExpandMore as ExpandMoreIcon,
+  Forum as ForumIcon
+} from '@material-ui/icons'
 import React from 'react'
 import Comment from './Comment'
 
@@ -25,4 +37,28 @@ function CommentSystem ({ title }) {
   )
 }
 
-export default CommentSystem
+const useStyles = makeStyles({
+  metaicon: {
+    verticalAlign: 'sub',
+  },
+})
+function CommentComponent ({ title }) {
+  const classes = useStyles()
+  return (
+    <Accordion variant="outlined" defaultExpanded>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="comment"
+      >
+        <Typography><ForumIcon fontSize="small" className={classes.metaicon} />{' 评论'}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Container>
+          <CommentSystem title={title} />
+        </Container>
+      </AccordionDetails>
+    </Accordion>
+  )
+}
+
+export default CommentComponent
