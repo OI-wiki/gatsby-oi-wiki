336fad14b53dc67980dc390e9cc1305ea69108d4
sshwy <jy.cat@qq.com>
2021-02-14 21:20:22 +0800

    妈妈再也不用担心 this file should be MANUALLY inserted into html.js 啦

--- gatsby-theme-oi-wiki/static/script.js
+++ gatsby-theme-oi-wiki/static/script.js
@@ -1,20 +1,24 @@
-'use strict'
+export default function () {
+  'use strict'
+  window.onthemechange = function (settings) {
+    const fallback = localStorage.settings &&
+      JSON.parse(localStorage.settings).darkMode.type
+    const darkOpt = settings !== undefined ? settings.darkMode.type : fallback
+    let themeClass = 'themeAuto'
+    let secondaryColorId = settings && settings.theme && settings.theme.secondary ? settings.theme.secondary : '3'
+    if (darkOpt === undefined || darkOpt === 'user-preference') {
+      // themeClass = 'themeAuto'
+    } else {
+      themeClass = darkOpt === 'always-on' ? 'themeDark' : 'themeLight'
+    }
+    const docEl = document.querySelector('html')
 
-window.onthemechange = function (settings) {
-  const fallback = localStorage.settings &&
-    JSON.parse(localStorage.settings).darkMode.type
-  const darkOpt = settings !== undefined ? settings.darkMode.type : fallback
-  let themeClass = 'themeAuto'
-  if (darkOpt === undefined || darkOpt === 'user-preference') {
-    // themeClass = 'themeAuto'
-  } else {
-    themeClass = darkOpt === 'always-on' ? 'themeDark' : 'themeLight'
+    docEl.className = ""
+    // docEl.classList.remove('themeLight', 'themeDark', 'themeAuto')
+    docEl.classList.add(themeClass)
+    docEl.classList.add('secondaryColor' + secondaryColorId)
   }
-  const docEl = document.querySelector('html')
-  docEl.classList.remove('themeLight', 'themeDark', 'themeAuto')
-  docEl.classList.add(themeClass)
+
+  window.onthemechange()
 }
-
-window.onthemechange()
-
 // this file should be MANUALLY inserted into html.js
