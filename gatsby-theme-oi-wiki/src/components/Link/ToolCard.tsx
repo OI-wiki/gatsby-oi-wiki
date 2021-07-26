c990c6fdd449aedf454032da6421ceb899017c9c
SkyeYoung <iskyex@outlook.com>
2021-07-26 14:11:53 +0800

    refactor(Link): add types, optimize functions
    But there is still a `<div> cannot appear as a descendant of <p>` problem that need to
    be fixed in ToolCard.tsx

--- gatsby-theme-oi-wiki/src/components/Link/ToolCard.tsx
+++ gatsby-theme-oi-wiki/src/components/Link/ToolCard.tsx
@@ -1,18 +1,17 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { getElementSize, getElementViewPosition, Position, Size } from './utils'
-import {
-  Card,
-  CardContent,
-  Fade,
-  CircularProgress,
-  makeStyles,
-} from '@material-ui/core'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { Alert } from '@material-ui/lab'
-import { PreviewData, FetchStatus } from './LinkTooltip'
-import { useDelay } from './hooks'
+import { Card, CardContent, CircularProgress, Fade, makeStyles } from '@material-ui/core'
 import { Link as GatsbyLink } from 'gatsby'
+
+import { FetchStatus, PreviewData } from './LinkTooltip'
+import { useDelay } from './hooks'
+import { getElementSize, getElementViewPosition, Position, Size } from './utils'
+import { Nullable } from '../../types/common'
+import clsx from 'clsx'
+
 const lines = 4
 const lineHeight = 1.5
+const cardDis = '2rem'
 const useStyles = makeStyles((theme) => ({
   fade: {
     lineHeight: `${lineHeight}em`,
@@ -27,145 +26,158 @@ const useStyles = makeStyles((theme) => ({
       right: 0,
       width: '30%',
       height: '1.5em',
-      background: (theme.palette as any).fadeTextBackground,
+      background: theme.palette.fadeTextBackground,
     },
   },
+  toolCard: {
+    display: 'inline-block',
+    position: 'absolute',
+    zIndex: 9999,
+    width: '320px',
+    left: 0,
+    top: cardDis,
+  },
+  aboveMedian: {
+    top: 'initial',
+    bottom: cardDis,
+  },
+  cardContent: {
+    display: 'flex',
+  },
+  fetching: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+
 }))
-type PositionAndSize = {
+
+interface PositionAndSize {
   pos: Position,
   size: Size,
 }
-function adjustElementPosition (element: HTMLElement, { pos, size }: PositionAndSize) : void {
-  if (!element) return
-  const viewport = {
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight,
-  }
-  const linkWidth = element.parentElement?.parentElement?.offsetWidth || 0
-  function setLower (): void {
-    element.style.removeProperty('bottom')
-    element.style.setProperty('top', '2em')
-  }
-  function setUpper (): void {
-    element.style.removeProperty('top')
-    element.style.setProperty('bottom', '2em')
-  }
-  function setRight (): void { // 控制横坐标
-    element.style.removeProperty('right')
-    let offset = 0
-    offset = Math.max(offset, -pos.x + 12) // 不能超过屏幕左边
-    offset = Math.min(offset, -pos.x + Math.max(0, viewport.width - size.width) - 12) // 不能超过屏幕右边
-    element.style.setProperty('left', `${offset}px`)
-  }
-  function setLeft (): void { // 控制横坐标
-    element.style.removeProperty('left')
-    let offset = 0
-    offset = Math.min(offset, pos.x + linkWidth - size.width - 12) // 不能超过屏幕左边
-    element.style.setProperty('right', `${offset}px`)
-  }
-  if (pos.y < viewport.height / 2) { // 位于上半部分
-    setLower()
-  } else {
-    setUpper()
-  }
-  if (pos.x + linkWidth / 2 < viewport.width / 2) {
-    setRight()
-  } else {
-    setLeft()
-  }
-  element.style.setProperty('max-width', `${viewport.width - 24}px`)
-}
 
-type Props = {
-  children: any,
-  content: PreviewData,
+
+export interface ToolCardProps {
+  content: Nullable<PreviewData>,
   status: FetchStatus,
   onOpen?: () => void,
-  onHover?: () => void, // 不会延迟执行
+  /** 不会延迟执行 */
+  onHover?: () => void,
   openDelay?: number,
   closeDelay?: number,
   to: string,
 }
-const ToolCard: React.FC<Props> = function (props: Props) {
+
+const ToolCard: React.FC<ToolCardProps> = (props) => {
   const classes = useStyles()
-  const { children, content, status } = props
-  const closeDelay = props.closeDelay || 0
-  const openDelay = props.openDelay || 0
+  const { children, content, status, closeDelay = 0, openDelay = 0, onHover } = props
   const [open, setOpen] = useState(false)
-  const poperRef = useRef(null)
+
+  const popperRef = useRef<HTMLElement>()
+  const position = useRef<Nullable<PositionAndSize>>(null)
   const [onOpen, onClose] = useDelay(() => {
     setOpen(true)
-    props.onOpen && props.onOpen()
+    props.onOpen?.()
+    onHover?.()
   }, () => {
     setOpen(false)
   }, openDelay, closeDelay)
-  const position = useRef(null)
+
+  const adjustElementPosition = useCallback((element: HTMLElement, { pos, size }: PositionAndSize): void => {
+    if (!element) return
+
+    const viewport = {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+    }
+
+    const linkWidth = element.parentElement?.parentElement?.offsetWidth || 0
+
+    // 控制横坐标
+    function setRight(): void {
+      element.style.removeProperty('right')
+      let offset = 0
+      // 不能超过屏幕左边
+      offset = Math.max(offset, -pos.x + 12)
+      // 不能超过屏幕右边
+      offset = Math.min(offset, -pos.x + Math.max(0, viewport.width - size.width) - 12)
+      element.style.setProperty('left', `${offset}px`)
+    }
+
+    // 控制横坐标
+    function setLeft(): void {
+      element.style.removeProperty('left')
+      let offset = 0
+      // 不能超过屏幕左边
+      offset = Math.min(offset, pos.x + linkWidth - size.width - 12)
+      element.style.setProperty('right', `${offset}px`)
+    }
+
+    element.classList.toggle(classes.aboveMedian, pos.y > viewport.height / 2)
+
+    // 位于左半部分
+    if (pos.x + linkWidth / 2 < viewport.width / 2) {
+      setRight()
+    } else {
+      setLeft()
+    }
+
+    element.style.setProperty('max-width', `${viewport.width - 24}px`)
+  }, [classes.aboveMedian])
 
   useEffect(() => {
-    if (open) {
+    if (open && popperRef.current) {
       const data: PositionAndSize = {
-        pos: getElementViewPosition(poperRef.current.parentElement),
-        size: getElementSize(poperRef.current),
+        pos: getElementViewPosition(popperRef.current.parentElement as HTMLElement),
+        size: getElementSize(popperRef.current),
       }
       position.current = data
-      adjustElementPosition(poperRef.current, data)
+      adjustElementPosition(popperRef.current, data)
     }
-  }, [open, content])
+  }, [open, content, popperRef, adjustElementPosition])
 
-  return (
-    <span
-      style={{
-        position: 'relative',
-        display: 'inline-block',
-      }}
-      onMouseEnter={() => {
-        onOpen()
-        props.onHover && props.onHover()
-      }}
-      onMouseLeave={() => onClose()}
-    >
-      <GatsbyLink to={props.to}><Fade in={open}>
-        <Card
-          className="toolcard"
-          elevation={3}
-          style={{
-            position: 'absolute',
-            zIndex: 9999,
-            width: '320px',
-            bottom: '2em',
-            left: 0,
-          }}
-          ref={poperRef}
-        >
-          {
-            (status === 'fetching' || status === 'not_fetched') &&
-            <CardContent style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }} >
-              <CircularProgress></CircularProgress>
-            </CardContent>
-          }
-          {status === 'fetched' &&
-            <CardContent>
-              <div className={classes.fade}>
-                <strong>
-                  {content.title + ' '}
-                </strong>
-                {content.text}
-              </div>
-            </CardContent>
-          }
-          {
-            status === 'error' &&
-              <Alert severity="error">无法获取页面预览</Alert>
-          }
-        </Card>
-      </Fade></GatsbyLink>
-      {children}
+  return <span
+    style={{
+      position: 'relative',
+      display: 'inline-block',
+    }}
+    onMouseEnter={onOpen}
+    onMouseLeave={onClose}
+  >
+      <GatsbyLink to={props.to}>
+        <Fade in={open}>
+          <Card
+            component='span'
+            className={classes.toolCard}
+            elevation={3}
+            ref={popperRef}
+          >
+            {(() => {
+              switch (status) {
+                case 'not_fetched':
+                case 'fetching':
+                  return <CardContent component='span' className={clsx(classes.fetching, classes.cardContent)}>
+                    <CircularProgress/>
+                  </CardContent>
+                case 'fetched':
+                  return <CardContent component='span' className={classes.cardContent}>
+                    <span className={classes.fade}>
+                      <strong>{content?.title + ' '}</strong>
+                      {content?.text}
+                    </span>
+                  </CardContent>
+                case 'error':
+                default:
+                  return <Alert severity="error">无法获取页面预览</Alert>
+              }
+            })()}
+          </Card>
+        </Fade>
+      </GatsbyLink>
+    {children}
     </span>
-  )
 }
 
 export default ToolCard
