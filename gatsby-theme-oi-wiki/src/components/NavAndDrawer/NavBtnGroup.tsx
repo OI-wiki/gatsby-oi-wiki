0184ffb6a55557e16631723b7f97188537849a79
SkyeYoung <iskyex@outlook.com>
2021-07-28 14:32:56 +0800

    refactor(NavAndDrawer): add types

--- gatsby-theme-oi-wiki/src/components/NavAndDrawer/NavBtnGroup.tsx
+++ gatsby-theme-oi-wiki/src/components/NavAndDrawer/NavBtnGroup.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Tooltip, IconButton, makeStyles } from '@material-ui/core'
+import { IconButton, IconButtonProps, makeStyles, Tooltip } from '@material-ui/core'
 
 import {
+  GitHub as GitHubIcon,
   LibraryBooks as LibraryBooksIcon,
   LocalOffer as LocalOfferIcon,
-  GitHub as GitHubIcon,
   Settings as SettingsIcon,
+  SvgIconComponent,
 } from '@material-ui/icons'
 
 const useStyles = makeStyles({
@@ -15,32 +16,36 @@ const useStyles = makeStyles({
   },
 })
 
-function NavBtn ({ title, href, Icon, ...restProps }) {
+interface NavBtnProps extends IconButtonProps<'a', React.AnchorHTMLAttributes<HTMLAnchorElement>> {
+  title: string;
+  Icon: SvgIconComponent;
+}
+
+const NavBtn: React.FC<NavBtnProps> = (props) => {
   const classes = useStyles()
-  return (
-    <Tooltip title={title} placement="bottom" arrow>
-      <IconButton component="a" href={href} color="inherit" className={classes.navBtn} {...restProps}>
-        <Icon />
-      </IconButton>
-    </Tooltip>
-  )
+  const { title, href, Icon, ...restProps } = props
+  return <Tooltip title={title} placement="bottom" arrow>
+    <IconButton component='a' href={href} className={classes.navBtn} {...restProps}>
+      <Icon/>
+    </IconButton>
+  </Tooltip>
 }
 
 const OIWikiGithub = 'https://github.com/OI-wiki/OI-wiki'
 
-export default function NavBtnGroup () {
-  return (
-    <>
-      <NavBtn title="设置页" href="/settings" Icon={SettingsIcon} />
-      <NavBtn title="标签页" href="/tags" Icon={LocalOfferIcon} />
-      <NavBtn title="目录页" href="/pages" Icon={LibraryBooksIcon} />
-      <NavBtn
-        title="GitHub 存储库"
-        href={OIWikiGithub}
-        Icon={GitHubIcon}
-        target="_blank"
-        rel="noreferrer noopener"
-      />
-    </>
-  )
+const NavBtnGroup: React.FC = () => {
+  return <>
+    <NavBtn title="设置页" href="/settings" Icon={SettingsIcon}/>
+    <NavBtn title="标签页" href="/tags" Icon={LocalOfferIcon}/>
+    <NavBtn title="目录页" href="/pages" Icon={LibraryBooksIcon}/>
+    <NavBtn
+      title="GitHub 存储库"
+      href={OIWikiGithub}
+      Icon={GitHubIcon}
+      target="_blank"
+      rel="noreferrer noopener"
+    />
+  </>
 }
+
+export default NavBtnGroup
--- gatsby-theme-oi-wiki/src/components/NavAndDrawer/utils.tsx
+++ gatsby-theme-oi-wiki/src/components/NavAndDrawer/utils.tsx
@@ -1,25 +1,25 @@
+import { StrIndexObj } from '../../types/common'
+
 /**
  * Flatten JS object (keys and values) to a single depth object
  * (ref: https://stackoverflow.com/a/53739792)
- *
- * @param {*} ob
- * @return {*}  {Record<string, unknown>}
  */
-export function flattenObject (ob:any) :Record<string, unknown> {
-  const toReturn = {}
+export function flattenObject(ob: StrIndexObj<any>): StrIndexObj {
+  const toReturn: StrIndexObj = {}
 
   for (const i in ob) {
-    if (!Object.prototype.hasOwnProperty.call(ob, i)) continue
+    if (Object.prototype.hasOwnProperty.call(ob, i)) {
+      if ((typeof ob[i]) === 'object' && ob[i] !== null) {
+        const flatObject = flattenObject(ob[i])
 
-    if ((typeof ob[i]) === 'object' && ob[i] !== null) {
-      const flatObject = flattenObject(ob[i])
-      for (const x in flatObject) {
-        if (!Object.prototype.hasOwnProperty.call(flatObject, x)) continue
-
-        toReturn[i + '.' + x] = flatObject[x]
+        for (const x in flatObject) {
+          if (Object.prototype.hasOwnProperty.call(flatObject, x)) {
+            toReturn[i + '.' + x] = flatObject[x]
+          }
+        }
+      } else {
+        toReturn[i] = ob[i]
       }
-    } else {
-      toReturn[i] = ob[i]
     }
   }
   return toReturn
