c156544de7e82d1b466588822fec7299558d9b88
mgt <mgt@oi-wiki.org>
2021-08-23 13:51:54 +0800

    fix: add fallback for all theme settings

--- gatsby-theme-oi-wiki/static/script.js
+++ gatsby-theme-oi-wiki/static/script.js
@@ -1,14 +1,15 @@
 export default function () {
   'use strict'
-  window.onthemechange = function (settings) {
-    const fallback = localStorage.settings && JSON.parse(localStorage.settings).darkMode.type
-    const darkOpt = settings !== undefined ? settings.darkMode.type : fallback
+  window.onthemechange = function (new_settings) {
+    const fallback = localStorage.settings && JSON.parse(localStorage.settings)
+    const settings = new_settings || fallback
+    const darkOpt = settings && settings.darkMode && settings.darkMode.type ? settings.darkMode.type : 'user-preference'
     const secondaryColorId = settings && settings.theme && settings.theme.secondary ? settings.theme.secondary : '3'
     const monoFont = settings && settings.theme && settings.theme.fallbackMonoFont
     const monoFontClass = monoFont ? 'fallback' : 'mono'
     let themeClass = 'auto'
     if (darkOpt === undefined || darkOpt === 'user-preference') {
-      // themeClass = 'auto'
+      themeClass = 'auto'
     } else {
       themeClass = darkOpt === 'always-on' ? 'dark' : 'light'
     }
