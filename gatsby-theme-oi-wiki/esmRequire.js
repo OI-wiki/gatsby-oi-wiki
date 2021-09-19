a83e9f6aa74fc3584b66ca004edc2fb3fb4ad211
true_mogician <925269713@qq.com>
2021-09-19 23:32:01 +0800

    fix: esm require

--- gatsby-theme-oi-wiki/esmRequire.js
+++ gatsby-theme-oi-wiki/esmRequire.js
@@ -2,33 +2,23 @@
  * @file export compiled ES modules as a workaround before Gatsby properly handles it
  */
 
-const esm = require('esm')
-const fs = require('fs')
-const Module = require('module')
-
+const esm = require('esm');
+const fs = require('fs');
+const Module = require('module');
 
 // Node: bypass [ERR_REQUIRE_ESM]
-const orig = Module._extensions['.js']
+const backup = Module._extensions['.js'];
 Module._extensions['.js'] = function (module, filename) {
-  try {
-    return orig(module, filename)
-  } catch (e) {
-    if (e.code === 'ERR_REQUIRE_ESM') {
-      const content = fs.readFileSync(filename, 'utf8')
-      module._compile(content, filename)
-    }
-  }
-}
+	const content = fs.readFileSync(filename, 'utf8');
+	module._compile(content, filename);
+};
 
-const _esmRequire = esm(module, {
-  cjs: true,
-  mode: 'all',
-})
+const esmRequire = esm(module, {
+	cjs: true,
+	mode: 'all',
+});
 
 // don't pollute Module
-Module._extensions['.js'] = orig
+Module._extensions['.js'] = backup;
 
-
-module.exports = function esmRequire(id) {
-  return _esmRequire(id)
-}
+module.exports = esmRequire;
