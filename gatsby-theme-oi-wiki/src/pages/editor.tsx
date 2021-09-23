dc7c7a2e07d28cd595935ecee9249713cad6bdae
true_mogician <925269713@qq.com>
2021-09-23 19:36:50 +0800

    refactor: use react hooks

--- gatsby-theme-oi-wiki/src/pages/editor.tsx
+++ gatsby-theme-oi-wiki/src/pages/editor.tsx
@@ -1,21 +1,21 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import MarkdownEditor from '../components/MarkdownEditor';
 import StyledLayout from '../components/StyledLayout';
 
-export default class EditorPage extends React.Component {
-	render(): ReactNode {
-		return (
-			<StyledLayout
-				location={location}
-				noTitle={true}
-				noMeta={true}
-				noComment={true}
-				noToc={true}
-				overflow={true}
-				title="Markdown编辑器"
-			>
-				<MarkdownEditor value="" />
-			</StyledLayout>
-		);
-	}
-}
+const EditorPage: React.FC = (_) => {
+	return (
+		<StyledLayout
+			location={location}
+			noTitle={true}
+			noMeta={true}
+			noComment={true}
+			noToc={true}
+			overflow={true}
+			title="Markdown编辑器"
+		>
+			<MarkdownEditor value="" />
+		</StyledLayout>
+	);
+};
+
+export default EditorPage;
