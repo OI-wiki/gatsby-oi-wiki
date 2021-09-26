c6e39e8d4c5afbcd0145afd7f017a0ffd50f5e41
SkyeYoung <iskyex@outlook.com>
2021-09-26 23:17:00 +0800

    refactor(globalstyle): extract pseudoCodeStyle

--- gatsby-theme-oi-wiki/src/theme/globalStyle.ts
+++ gatsby-theme-oi-wiki/src/theme/globalStyle.ts
@@ -1,9 +1,10 @@
 import { css, SerializedStyles } from '@emotion/react'
 import { Theme } from '@mui/material/styles/createTheme'
+import pseudoCodeStyle from './pseudoCodeStyle'
 
 // the css here will be injected by the <GlobalStyles>
 // position: src/gatsby-func/WrapRootElement.tsx
-const globalStyle = (_theme: Theme): SerializedStyles => css`
+const globalStyle = (theme: Theme): SerializedStyles => css`
   :root {
     --code-block-font: "Fira Mono", "Menlo", "Monaco", "Consolas", "Andale Mono", "Ubuntu Mono", "Courier New", "Hack", "Fira Code", "Jetbrains Mono", monospace;
 
@@ -11,6 +12,8 @@ const globalStyle = (_theme: Theme): SerializedStyles => css`
       --code-block-font: monospace;
     }
   }
+
+  ${pseudoCodeStyle(theme)};
 `
 
 export default globalStyle
