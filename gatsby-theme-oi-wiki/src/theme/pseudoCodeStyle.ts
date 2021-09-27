b685f5b119f26c4c0ef66350761dea3d201213c7
SkyeYoung <iskyex@outlook.com>
2021-09-27 16:22:52 +0800

    refactor(pseudocodestyle): simplify css selectors

--- gatsby-theme-oi-wiki/src/theme/pseudoCodeStyle.ts
+++ gatsby-theme-oi-wiki/src/theme/pseudoCodeStyle.ts
@@ -7,76 +7,75 @@ const pseudoCodeStyle = (theme: Theme): SerializedStyles => css`
     font-size: 1em;
     font-weight: 100;
     -webkit-font-smoothing: antialiased !important;
-  }
 
-  .ps-root .ps-algorithm {
-    margin: 0.8em 0;
-    border-top: 3px solid ${theme.palette.text.secondary};
-    border-bottom: 2px solid rgba(var(--text-secondary));
-  }
+    .ps-algorithm {
+      margin: 0.8em 0;
+      border-top: 3px solid ${theme.palette.text.secondary};
+      border-bottom: 2px solid rgba(var(--text-secondary));
 
-  .ps-root .ps-algorithm.with-caption > .ps-line:first-child {
-    border-bottom: 2px solid ${theme.palette.text.secondary};
-    padding-top: .5em;
-  }
+      &.with-caption > .ps-line > span:first-of-type {
+        border-bottom: 2px solid ${theme.palette.text.secondary};
+        padding-top: .5em;
+      }
+    }
 
-  .ps-root .katex {
-    text-indent: 0;
-    font-size: 1em;
-  }
+    .ps-algorithmic.with-linenum .ps-line.ps-code {
+      text-indent: -1.6em;
 
-  .ps-root .MathJax,
-  .ps-root .MathJax_CHTML {
-    text-indent: 0;
-    font-size: 1em !important;
-  }
+      & > span {
+        text-indent: 0;
+      }
+    }
 
-  .ps-root .ps-line {
-    margin: 0;
-    padding: 0;
-    line-height: 1.2;
-  }
+    .katex {
+      text-indent: 0;
+      font-size: 1em;
+    }
 
-  .ps-root .ps-funcname {
-    font-family: MJXTEX, "Times New Roman", Times, serif;
-    font-weight: 400;
-    font-variant: small-caps;
-    font-style: normal;
-    text-transform: none;
-  }
+    .MathJax, .MathJax_CHTML {
+      text-indent: 0;
+      font-size: 1em !important;
+    }
 
-  .ps-root .ps-keyword {
-    font-family: MJXTEX-B, "Times New Roman", Times, serif;
-    font-weight: 700;
-    font-variant: normal;
-    font-style: normal;
-    text-transform: none;
-  }
+    .ps-line {
+      margin: 0;
+      padding: 0;
+      line-height: 1.2;
+    }
 
-  .ps-root .ps-comment {
-    font-family: MJXTEX, "Times New Roman", Times, serif;
-    font-weight: 400;
-    font-variant: normal;
-    font-style: normal;
-    text-transform: none;
-  }
+    .ps-keyword {
+      font-family: MJXTEX-B, "Times New Roman", Times, serif;
+      font-weight: 700;
+      font-variant: normal;
+      font-style: normal;
+      text-transform: none;
+    }
 
-  .ps-root .ps-linenum {
-    font-size: 0.8em;
-    line-height: 1em;
-    width: 1.6em;
-    text-align: right;
-    display: inline-block;
-    position: relative;
-    padding-right: 0.3em;
-  }
+    .ps-comment {
+      font-family: MJXTEX, "Times New Roman", Times, serif;
+      font-weight: 400;
+      font-variant: normal;
+      font-style: normal;
+      text-transform: none;
+    }
 
-  .ps-root .ps-algorithmic.with-linenum .ps-line.ps-code {
-    text-indent: -1.6em;
-  }
+    .ps-linenum {
+      font-size: 0.8em;
+      line-height: 1em;
+      width: 1.6em;
+      text-align: right;
+      display: inline-block;
+      position: relative;
+      padding-right: 0.3em;
+    }
 
-  .ps-root .ps-algorithmic.with-linenum .ps-line.ps-code > span {
-    text-indent: 0;
+    .ps-funcname {
+      font-family: MJXTEX, "Times New Roman", Times, serif;
+      font-weight: 400;
+      font-variant: small-caps;
+      font-style: normal;
+      text-transform: none;
+    }
   }
 `
 
