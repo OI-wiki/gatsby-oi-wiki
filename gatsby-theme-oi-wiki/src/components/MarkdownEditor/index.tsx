44544e7ab944003b8a27813c4c4fa1b4f6db4983
true_mogician <925269713@qq.com>
2021-09-28 19:02:34 +0800

    refactor: remove unused markdown extensions

--- gatsby-theme-oi-wiki/src/components/MarkdownEditor/index.tsx
+++ gatsby-theme-oi-wiki/src/components/MarkdownEditor/index.tsx
@@ -1,110 +1,57 @@
-import "bytemd/dist/index.css";
-import "./styles/override.css";
-import "./styles/highlight/override.css";
-import "./styles/code-mirror/vscode-dark.css";
-import "basic-type-extensions";
-import { BytemdPlugin } from "bytemd";
-import { Editor } from "@bytemd/react";
-import breaks from "@bytemd/plugin-breaks";
-import footnotes from "@bytemd/plugin-footnotes";
-import frontmatter from "@bytemd/plugin-frontmatter";
-import gemoji from "@bytemd/plugin-gemoji";
-import gfm from "@bytemd/plugin-gfm";
-import math from "@bytemd/plugin-math-ssr";
-import mediumZoom from "@bytemd/plugin-medium-zoom";
-import mermaid from "@bytemd/plugin-mermaid";
-import highlight from "@bytemd/plugin-highlight-ssr";
-import details from "./plugins/details";
-import pseudo from "./plugins/pseudo";
-import React, { useState, useEffect } from "react";
-
-interface MarkdownPlugin {
-	math: boolean;
-	details: boolean;
-	breaks: boolean;
-	footnotes: boolean;
-	frontmatter: boolean;
-	gemoji: boolean;
-	gfm: boolean;
-	highlight: boolean;
-	mediumZoom: boolean;
-	mermaid: boolean;
-}
+import 'bytemd/dist/index.css';
+import './styles/override.css';
+import './styles/highlight/override.css';
+import './styles/code-mirror/vscode-dark.css';
+import 'basic-type-extensions';
+import { BytemdPlugin } from 'bytemd';
+import { Editor } from '@bytemd/react';
+import gemoji from '@bytemd/plugin-gemoji';
+import frontmatter from '@bytemd/plugin-frontmatter';
+import math from '@bytemd/plugin-math-ssr';
+import highlight from '@bytemd/plugin-highlight-ssr';
+import details from './plugins/details';
+import pseudo from './plugins/pseudo';
+import React, { useState, useEffect } from 'react';
 
 interface MarkdownEditorProps {
 	value: string;
-	plugins?: Partial<MarkdownPlugin>;
+	onChange?: (value: string) => void;
 }
 
-const MarkdownEditor: React.FC<Partial<MarkdownEditorProps>> = (props) => {
-	const plugins: MarkdownPlugin = {
-		math: true,
-		highlight: true,
-		details: true,
-		breaks: false,
-		footnotes: false,
-		frontmatter: true,
-		gemoji: true,
-		gfm: false,
-		mediumZoom: false,
-		mermaid: false,
-		...props.plugins,
-	};
-	let theme: "dark" | "light" = "light";
-	const dataTheme = document
-		.getElementsByTagName("html")
-		.item(0)
-		?.getAttribute("data-theme");
-	if (dataTheme == "dark") theme = "dark";
-	else if (
-		dataTheme == "auto" &&
-		window.matchMedia("(prefers-color-scheme: dark)")
-	) {
-		theme = "dark";
+const MarkdownEditor: React.FC<Partial<MarkdownEditorProps>> = props => {
+	let theme: 'dark' | 'light' = 'light';
+	const dataTheme = document.getElementsByTagName('html').item(0)?.getAttribute('data-theme');
+	if (dataTheme == 'dark') theme = 'dark';
+	else if (dataTheme == 'auto' && window.matchMedia('(prefers-color-scheme: dark)')) {
+		theme = 'dark';
 	}
-	const enabledPlugins: BytemdPlugin[] = [];
-	enabledPlugins.push(pseudo());
-	if (plugins.math) enabledPlugins.push(math());
-	if (plugins.details) enabledPlugins.push(details());
-	if (plugins.breaks) enabledPlugins.push(breaks());
-	if (plugins.footnotes) enabledPlugins.push(footnotes());
-	if (plugins.frontmatter) enabledPlugins.push(frontmatter());
-	if (plugins.gemoji) enabledPlugins.push(gemoji());
-	if (plugins.gfm) enabledPlugins.push(gfm());
-	if (plugins.highlight) enabledPlugins.push(highlight());
-	if (plugins.mediumZoom) enabledPlugins.push(mediumZoom());
-	if (plugins.mermaid) enabledPlugins.push(mermaid());
-	const [value, setValue] = useState(props.value ?? "");
+	const enabledPlugins: BytemdPlugin[] = [pseudo(), math(), details(), frontmatter(), gemoji(), highlight()];
+	const [value, setValue] = useState(props.value ?? '');
 	useEffect(() => {
-		const containerEl = document
-			.getElementsByClassName("bytemd")
-			.item(0)?.parentElement;
+		const containerEl = document.getElementsByClassName('bytemd').item(0)?.parentElement;
 		if (containerEl != null) {
-			containerEl.style.flexGrow = "1";
-			containerEl.style.display = "flex";
-			containerEl.style.flexDirection = "column";
+			containerEl.style.flexGrow = '1';
+			containerEl.style.display = 'flex';
+			containerEl.style.flexDirection = 'column';
 		}
-	});
+	}, []);
+	useEffect(() => setValue(props.value ?? ''), [props.value]);
 	return (
 		<>
-			{plugins.math && (
-				<link
-					rel="stylesheet"
-					href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
-					integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
-					crossOrigin="anonymous"
-				/>
-			)}
-			{plugins.highlight && (
-				<link
-					rel="stylesheet"
-					href={
-						"https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/styles/" +
-						(theme == "dark" ? "vs2015" : "vs") +
-						".min.css"
-					}
-				/>
-			)}
+			<link
+				rel="stylesheet"
+				href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
+				integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
+				crossOrigin="anonymous"
+			/>
+			<link
+				rel="stylesheet"
+				href={
+					'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/styles/' +
+					(theme == 'dark' ? 'vs2015' : 'vs') +
+					'.min.css'
+				}
+			/>
 			<Editor
 				value={value}
 				plugins={enabledPlugins}
@@ -113,7 +60,10 @@ const MarkdownEditor: React.FC<Partial<MarkdownEditorProps>> = (props) => {
 					smartIndent: true,
 				}}
 				previewDebounce={500}
-				onChange={(v) => setValue(v)}
+				onChange={v => {
+					setValue(v);
+					props.onChange?.(v);
+				}}
 			/>
 		</>
 	);
