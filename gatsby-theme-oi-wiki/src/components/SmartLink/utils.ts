601381412dcdeff8e6c36c09cfd4de3aedb4278d
SkyeYoung <iskyex@outlook.com>
2021-09-28 16:05:26 +0800

    refactor(smartlink): simplify getElementViewPosition
    Co-authored-by: 代小呆 <diauweb@live.com>

--- gatsby-theme-oi-wiki/src/components/SmartLink/utils.ts
+++ gatsby-theme-oi-wiki/src/components/SmartLink/utils.ts
@@ -6,18 +6,7 @@ export type Position = Pick<DOMRect, 'x' | 'y'>
  * 获取元素的绝对位置坐标（相对于浏览器视区左上角）
  */
 export const getElementViewPosition = (el: Nullable<HTMLElement>): Position => {
-  if (el === null) {
-    return {
-      x: 0,
-      y: 0,
-    }
-  } else {
-    const rect = el.getBoundingClientRect()
-    return {
-      x: rect.x || rect.top,
-      y: rect.y || rect.bottom,
-    }
-  }
+  return el?.getBoundingClientRect() ?? { x: 0, y: 0 }
 }
 
 export type Size = Pick<DOMRect, 'width' | 'height'>
