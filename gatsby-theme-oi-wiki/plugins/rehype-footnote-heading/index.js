95d584b3fe387f43b1e08db80dccb3b8fb94f439
mgt <mgt@oi-wiki.org>
2021-11-13 15:24:41 +0800

    fix: fix footnote heading plugin

--- gatsby-theme-oi-wiki/plugins/rehype-footnote-heading/index.js
+++ gatsby-theme-oi-wiki/plugins/rehype-footnote-heading/index.js
@@ -5,12 +5,14 @@ module.exports = function () {
     visit(ast, 'element', function (node, index, parent) {
       if (node.tagName !== 'h2') return
       if (node.properties['id'] !== 'footnote-label') return
-
-      parent[index] = {
-        type: 'element',
-        tagName: 'hr',
-        properties: node.properties
+      const properties = node.properties
+      for (const i of Object.keys(node)) {
+        delete node[i]
       }
+      node.type = 'element'
+      node.tagName = 'hr'
+      node.properties = properties
+      node.properties['className'].push('MuiDivider-root', 'makeStyles-divider-4')
     })
   }
 }
